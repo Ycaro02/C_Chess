d845eb1207a771eb768401c3cf4041d19b20e5e2
Ycaro02 <Ycaro02@hotmail.fr>
2024-08-16 21:06:52 +0200

    Basic init for bitboard and chessBoard

---
+++ include/chess.h
@@ -0,0 +1,72 @@
+#ifndef CHESS_H
+#define CHESS_H
+
+#include "../libft/libft.h"
+
+/* Typedef for Bitboard type just unsigned long long (64 bits) */
+typedef u64 Bitboard;
+
+/* Enum for chess tile */
+enum e_chess_tile {
+	A1, B1, C1, D1, E1, F1, G1, H1,
+	A2, B2, C2, D2, E2, F2, G2, H2,
+	A3, B3, C3, D3, E3, F3, G3, H3,
+	A4, B4, C4, D4, E4, F4, G4, H4,
+	A5, B5, C5, D5, E5, F5, G5, H5,
+	A6, B6, C6, D6, E6, F6, G6, H6,
+	A7, B7, C7, D7, E7, F7, G7, H7,
+	A8, B8, C8, D8, E8, F8, G8, H8
+};
+
+/* Start white piece position */
+#define START_WHITE_PAWNS 0x000000000000FF00ULL
+#define START_WHITE_KNIGHTS (1ULL << B1 | 1ULL << G1)
+#define START_WHITE_BISHOPS (1ULL << C1 | 1ULL << F1)
+#define START_WHITE_ROOKS (1ULL << A1 | 1ULL << H1)
+#define START_WHITE_QUEENS (1ULL << D1)
+#define START_WHITE_KING (1ULL << E1)
+
+/* Start black piece position */
+#define START_BLACK_PAWNS 0x00FF000000000000ULL
+#define START_BLACK_KNIGHTS (1ULL << B8 | 1ULL << G8)
+#define START_BLACK_BISHOPS (1ULL << C8 | 1ULL << F8)
+#define START_BLACK_ROOKS (1ULL << A8 | 1ULL << H8)
+#define START_BLACK_QUEENS (1ULL << D8)
+#define START_BLACK_KING (1ULL << E8)
+
+/* Enum for chess piece */
+enum e_chess_piece {
+	WHITE_PAWN, WHITE_KNIGHT, WHITE_BISHOP, WHITE_ROOK, WHITE_QUEEN, WHITE_KING,
+	BLACK_PAWN, BLACK_KNIGHT, BLACK_BISHOP, BLACK_ROOK, BLACK_QUEEN, BLACK_KING
+};
+
+
+/* ChessBoard struct */
+struct s_chess_board {
+	/* Board 1 for occupied, 0 for empty */
+	Bitboard occupied;
+
+	/* White pieces */
+	Bitboard white_pawns;
+	Bitboard white_knights;
+	Bitboard white_bishops;
+	Bitboard white_rooks;
+	Bitboard white_queens;
+	Bitboard white_king;
+
+	/* Black pieces */
+	Bitboard black_pawns;
+	Bitboard black_knights;
+	Bitboard black_bishops;
+	Bitboard black_rooks;
+	Bitboard black_queens;
+	Bitboard black_king;
+};
+
+/* Typedef for ChessBoard struct */
+typedef struct s_chess_board ChessBoard;
+
+
+
+
+#endif /* CHESS_H */
--- src/main.c
+++ src/main.c
@@ -1,5 +1,62 @@
-#include "../libft/libft.h"
+#include "../include/chess.h"
+
+/* Mask for A and H columns */
+// #define FILE_A = 0x0101010101010101ULL;
+// #define FILE_H = 0x8080808080808080ULL;
+
+/* Update occupied bitboard */
+void update_occupied(ChessBoard *board) {
+	board->occupied =	board->white_pawns | board->white_knights | board->white_bishops
+						| board->white_rooks | board->white_queens | board->white_king
+						| board->black_pawns | board->black_knights | board->black_bishops
+						| board->black_rooks | board->black_queens | board->black_king;
+}
+
+void init_board(ChessBoard *board) {
+	board->white_pawns = START_WHITE_PAWNS;
+	board->white_knights = START_WHITE_KNIGHTS;
+	board->white_bishops = START_WHITE_BISHOPS;
+	board->white_rooks = START_WHITE_ROOKS;
+	board->white_queens = START_WHITE_QUEENS;
+	board->white_king = START_WHITE_KING;
+	
+	board->black_pawns = START_BLACK_PAWNS;
+	board->black_knights = START_BLACK_KNIGHTS;
+	board->black_bishops = START_BLACK_BISHOPS;
+	board->black_rooks = START_BLACK_ROOKS;
+	board->black_queens = START_BLACK_QUEENS;
+	board->black_king = START_BLACK_KING;
+
+	update_occupied(board);
+}
+
+/* Display bitboard */
+void display_bitboard(Bitboard board) {
+	for (int i = 0; i < 64; i++) {
+		if (i % 8 == 0) {
+			ft_printf_fd(1, "\n");
+		}
+		ft_printf_fd(1, "%d", (board >> i) & 1);
+	}
+	ft_printf_fd(1, "\n");
+}
 
 int main(void) {
+	ChessBoard board = {0};
+
+	init_board(&board);
+
+	display_bitboard(board.occupied);
+
 	ft_printf_fd(1, "Hello From C_Chess\n");
 }
+
+/* Set and clear bit */
+// void set_bit(Bitboard *board, int index) {
+// 	*board |= 1ULL << index;
+// }
+
+// void clear_bit(Bitboard *board, int index) {
+// 	*board &= ~(1ULL << index);
+// }
+
