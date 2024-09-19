1a6c9cf608a5bc3c9ffe7fec5c71c9cb3787ca21
Ycaro02 <Ycaro02@hotmail.fr>
2024-09-19 15:26:09 +0200

    Add gradle compile option to see deprecated function

--- android/chess_app/build_android.sh
+++ android/chess_app/build_android.sh
@@ -17,13 +17,19 @@ echo "Copying libs to app/src/main/jniLibs"
 
 export APP_ALLOW_MISSING_DEPS=true && export ANDROID_HOME=~/Android/Sdk
 
+./gradlew clean
 
 if [ "$1" == "clean" ]; then
-	./gradlew clean
 	exit 0
 fi
 
-./gradlew clean
+if [ "$1" == "release" ]; then
+	./gradlew assembleRelease
+	cp app/build/outputs/apk/release/app-release-unsigned.apk apk_release/chess_app.apk
+	echo "Release APK generated at apk_release/chess_app.apk"
+	exit 0
+fi
+
 ./gradlew assembleDebug
 
 cp app/build/outputs/apk/debug/app-debug.apk apk_release/chess_app.apk
