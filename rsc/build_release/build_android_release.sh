e144937c3c6c727618694a7aeac59711e279808d
Ycaro02 <Ycaro02@hotmail.fr>
2024-09-22 15:04:28 +0200

    Clean android script to be modulat

--- rsc/build_release/build_android_release.sh
+++ rsc/build_release/build_android_release.sh
@@ -5,16 +5,16 @@
 
 source rsc/sh/color.sh
 
-# Variables
+# Global Variables
 REPO="Ycaro02/C_Chess"
-RELEASE_NAME="C_Chess_Android"
 GITHUB_TOKEN=$(cat ~/.tok_C_chess)
-APK_PATH="android/chess_app/apk_release/chess_app.apk"
-APK_NAME="chess_app.apk"
+
+
 
 # Get the version from the version file
+# $1: version file path
 function get_version {
-	local version_file="rsc/version/version.h"
+	local version_file="${1}"
 	if [ ! -f "${version_file}" ]; then
 		display_color_msg ${RED} "Version file not found: ${version_file}"
 		exit 1
@@ -33,6 +33,7 @@ function compile_apk {
 }
 
 # List all releases and filter by name, to update the release if it already exists
+# $1: release names
 function get_release_by_name() {
 	local release_name="${1}"
 	local release_id=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
@@ -40,30 +41,57 @@ function get_release_by_name() {
 	echo "${release_id}"
 }
 
+# Get the last word of a string by a separator
+# $1: string
+# $2: separator
+function get_last_word_by_sep {
+	local string="${1}"
+	local separator="${2}"
+	echo $(echo "${string}" | rev | cut -d"${separator}" -f1 | rev)
+}
+
+# Create or update a release on GitHub, and upload the data file
+# $1: release name
+# $2: release tag
+# $3: data file path
 function update_release {
 
-	local release_id=$(get_release_by_name "${RELEASE_NAME}")
+	local release_name="${1}"
+	local release_tag="${2}"
+	local data_path="${3}"
+
+	local file_name=$(get_last_word_by_sep "${data_path}" "/")
+	display_color_msg ${YELLOW} "Uploading APK file: ${file_name} from ${data_path}"
+
+	local version=$(get_last_word_by_sep "${release_tag}" "_")
+	display_color_msg ${YELLOW} "Version: ${version} from release_tag ${release_tag}"
+
+	local release_id=$(get_release_by_name "${release_name}")
+
+
+
+	local response=""
 
 	if [ "${release_id}" == "" ]; then
 		# Create a new release
 		display_color_msg ${LIGHT_BLUE} "Creating a new release..."
-		RESPONSE=$(curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
+		response=$(curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
 			-H "Content-Type: application/json" \
-			-d "{\"tag_name\": \"${RELEASE_TAG}\", \"name\": \"${RELEASE_NAME}\", \"body\": \"Release of ${RELEASE_NAME}\", \"draft\": false, \"prerelease\": false}" \
+			-d "{\"tag_name\": \"${release_tag}\", \"name\": \"${release_name}\", \"body\": \"Release of ${release_name}: Version ${version}\", \"draft\": false, \"prerelease\": false}" \
 			"https://api.github.com/repos/${REPO}/releases")
-		release_id=$(echo "${RESPONSE}" | jq -r '.id')
+		release_id=$(echo "${response}" | jq -r '.id')
 	else
 		# Update the existing release
-		display_color_msg ${LIGHT_BLUE} "Updating the existing release : ID: ${release_id}, TAG: ${RELEASE_TAG}"
-		RESPONSE=$(curl -s -X PATCH -H "Authorization: token ${GITHUB_TOKEN}" \
+		display_color_msg ${LIGHT_BLUE} "Updating the existing release : ID: ${release_id}, TAG: ${release_tag}"
+		response=$(curl -s -X PATCH -H "Authorization: token ${GITHUB_TOKEN}" \
 			-H "Content-Type: application/json" \
-			-d "{\"tag_name\": \"${RELEASE_TAG}\", \"name\": \"$RELEASE_NAME\", \"body\": \"Updated release of ${RELEASE_NAME}: Version ${VERSION}\", \"draft\": false, \"prerelease\": false}" \
+			-d "{\"tag_name\": \"${release_tag}\", \"name\": \"${release_name}\", \"body\": \"Updated release of ${release_name}: Version ${version}\", \"draft\": false, \"prerelease\": false}" \
 			"https://api.github.com/repos/${REPO}/releases/${release_id}")
 	fi
 
 	# Get the asset ID of the existing APK file
 	ASSET_ID=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
-		"https://api.github.com/repos/${REPO}/releases/${release_id}/assets" | jq -r ".[] | select(.name == \"${APK_NAME}\") | .id")
+		"https://api.github.com/repos/${REPO}/releases/${release_id}/assets" | jq -r ".[] | select(.name == \"${file_name}\") | .id")
 
 	# Delete the existing APK file if it exists
 	if [ "${ASSET_ID}" != "" ]; then
@@ -76,13 +104,15 @@ function update_release {
 	display_color_msg ${LIGHT_BLUE} "Uploading the new APK file..."
 	curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
 		-H "Content-Type: application/vnd.android.package-archive" \
-		--data-binary @"${APK_PATH}" \
-		"https://uploads.github.com/repos/${REPO}/releases/${release_id}/assets?name=${APK_NAME}"
+		--data-binary @"${data_path}" \
+		"https://uploads.github.com/repos/${REPO}/releases/${release_id}/assets?name=${file_name}"
 
 	display_color_msg ${GREEN} "\nAndroid release APK uploaded to GitHub!"
 
 }
 
+# Remove the old release tags
+# $1: tag name
 function remove_old_release_tags {
 	local tag_name="${1}"
 	# List all tags and delete those starting with ${tag_name}
@@ -98,7 +128,9 @@ function remove_old_release_tags {
 	done
 }
 
-function remove_old_release {
+# Remove the old release by name
+# $1: release name
+function remove_old_release_name {
 	local release_name="${1}"
 	local release_id=$(get_release_by_name "${release_name}")
 
@@ -109,22 +141,31 @@ function remove_old_release {
 	fi
 }
 
-START_TAG="AndroidRelease_"
 
-VERSION=$(get_version)
-display_color_msg ${YELLOW} "Version: ${VERSION}"
-RELEASE_TAG="${START_TAG}${VERSION}"
+function android_update_release {
+	local release_name="C_Chess_Android"
+	local start_tag="AndroidRelease_"
+	local apk_path="android/chess_app/apk_release/chess_app.apk"
 
 
-compile_apk
+	local version=$(get_version "rsc/version/version.h")
+	display_color_msg ${YELLOW} "Version: ${version}"
+	local release_tag="${start_tag}${version}"
 
-# Check if the APK exists
-if [ ! -f "${APK_PATH}" ]; then
-    display_color_msg ${RED} "APK file not found: ${APK_PATH}"
-    exit 1
-fi
 
-remove_old_release_tags ${START_TAG}
-remove_old_release ${RELEASE_NAME}
+	compile_apk
 
-update_release
+	# Check if the APK exists
+	if [ ! -f "${apk_path}" ]; then
+		display_color_msg ${RED} "APK file not found: ${apk_path}"
+		exit 1
+	fi
+
+	remove_old_release_tags ${start_tag}
+	remove_old_release_name ${release_name}
+
+	update_release ${release_name} ${release_tag} ${apk_path}
+}
+
+android_update_release
+
