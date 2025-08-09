063c7410691b51ba984b7dd1751552813ba503f2
Ycaro02 <ycaro02@hotmail.fr>
2025-08-09 12:13:41 +0000

    Add docker run and script for android building in container

--- android/chess_app/build_android.sh
+++ android/chess_app/build_android.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
+
 function update_lib_dir {
 	local archi="${1}"
 
 	mkdir -p app/src/main/jniLibs/${archi}
 	echo "Updating ${archi} lib dir"
-	cp ../android_lib/${archi}/* app/src/main/jniLibs/${archi}
+	cp ${ANDROID_WORK_DIR}/android_lib/${archi}/* app/src/main/jniLibs/${archi}
 }
 
 update_lib_dir "arm64-v8a"
@@ -15,7 +17,8 @@ update_lib_dir "x86_64"
 
 echo "Copying libs to app/src/main/jniLibs"
 
-export APP_ALLOW_MISSING_DEPS=true && export ANDROID_HOME=~/Android/Sdk
+export APP_ALLOW_MISSING_DEPS=true
+# export APP_ALLOW_MISSING_DEPS=true && export ANDROID_HOME=~/Android/Sdk
 
 ./gradlew clean
 
